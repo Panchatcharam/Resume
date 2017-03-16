66603342afbff8e376cf75c27777be271b7baa09
Murugesan <Pancha.Murugesan@Emerson.com>
2017-03-16 16:39:12 +0800

    updated css and included php script to send an email

---
+++ scripts/contact.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $from = 'From: Unknown'; 
+    $to = 'mrpanchu2003@gmail.com'; 
+    $subject = 'Hello';
+//    $human = $_POST['human'];
+			
+    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+				
+//    if ($_POST['submit'] && $human == '4') {				 
+//        if (mail ($to, $subject, $body, $from)) { 
+//	    echo '<p>Your message has been sent!</p>';
+//	} else { 
+//	    echo '<p>Something went wrong, go back and try again!</p>'; 
+//	} 
+//    } else if ($_POST['submit'] && $human != '4') {
+//	echo '<p>You answered the anti-spam question incorrectly!</p>';
+//    }
+
+    if (mail ($to, $subject, $body, $from)) 
+    { 
+        echo '<p>Your message has been sent!</p>';
+    } 
+    else 
+    { 
+        echo '<p>Something went wrong, go back and try again!</p>'; 
+    } 
+?>
+
+</body>
+</html>
